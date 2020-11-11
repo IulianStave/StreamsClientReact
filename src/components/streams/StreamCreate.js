8bd316c409099008b3c2b7606e2af3f0df8208fe
IulianStave <iulian.staverescu@gmail.com>
2020-11-11 20:52:45 +0200

    Destructure input

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,24 +2,23 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class StreamCreate extends React.Component {
-  
-  
-  renderInput(formProps) {
-    console.log(formProps);
+  // renderInput(formProps) {
+  renderInput({ input }) {
     return (
-      <input 
-        onChange={formProps.input.onChange}
-        value={formProps.input.value}
-      
+      // <input {...formProps.input}
+      <input
+        {...input}
+        // onChange={formProps.input.onChange}
+        // value={formProps.input.value}
       />
-      );
+    );
   }
-  
+
   render() {
     return (
       <form>
-        <Field name="title" component={this.renderInput}/>
-        <Field name="description" component={this.renderInput}/>
+        <Field name="title" component={this.renderInput} />
+        <Field name="description" component={this.renderInput} />
       </form>
     );
   }
