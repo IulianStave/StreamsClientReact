1da17e2e7d66aa13ea6599b37ac930ad043109b0
IulianStave <iulian.staverescu@gmail.com>
2020-11-11 19:25:13 +0200

    Add reducer as formReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
-import { reducer } from 'redux-form';
+import { reducer as formReducer} from 'redux-form';
 
+// reducer as formReducer is installed frokm redux-form 
+// we use named import
 export default combineReducers ({
   auth: authReducer,
-  form: reducer
+  form: formReducer
 });
