dbf893f277f287b1b923e65138d3c72301157d57
IulianStave <iulian.staverescu@gmail.com>
2020-11-16 18:50:38 +0200

    Add streamReducer using lodash library

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 import { reducer as formReducer} from 'redux-form';
+import streamReducer from './streamReducer';
+
 
 // reducer as formReducer is installed frokm redux-form 
 // we use named import
 export default combineReducers ({
   auth: authReducer,
+  streams: streamReducer,
   form: formReducer
 });
