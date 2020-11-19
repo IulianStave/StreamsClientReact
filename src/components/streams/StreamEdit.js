3275eea8667aeafa99aaf50a59fee1d687093f33
IulianStave <iulian.staverescu@gmail.com>
2020-11-19 18:45:28 +0200

    Wire up the action creator fetchStream via connect

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,18 +1,27 @@
 import React from "react";
 import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
 
 class StreamEdit extends React.Component {
   componentDidMount() {
     // We use ownProps property match -> params -> id
     // passed by default when rendering in Route component
     // based on path
+
+    // Trigger an action creator fetchStream wired up to the class 
+    // component via connect to ensure component isolation
+    // No mather how the user gets to this page
+    // the component StreamEdit will fetch his own data - the stream
+    // from selected from the url (based on id)
     this.props.fetchStream(this.props.match.params.id);
   }
-  
-  
+
   render() {
     console.log(this.props);
-    return <div>Edit a stream</div>;
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+    return <div>{this.props.stream.title}</div>;
   }
 }
 
@@ -20,4 +29,4 @@ const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps)(StreamEdit);
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
