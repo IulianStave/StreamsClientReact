435be6207b2c74864cd60c30d2d621fda40b8488
IulianStave <iulian.staverescu@gmail.com>
2020-11-20 18:07:19 +0200

    Refactor StreamEdit to pass a prop from  redux-form called initialValues

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,27 +1,33 @@
 import React from "react";
 import { connect } from "react-redux";
-import { fetchStream } from "../../actions";
+import { fetchStream, editStream } from "../../actions";
+import StreamForm from "./StreamForm";
 
 class StreamEdit extends React.Component {
   componentDidMount() {
-    // We use ownProps property match -> params -> id
-    // passed by default when rendering in Route component
-    // based on path
-
-    // Trigger an action creator fetchStream wired up to the class 
-    // component via connect to ensure component isolation
-    // No mather how the user gets to this page
-    // the component StreamEdit will fetch his own data - the stream
-    // from selected from the url (based on id)
     this.props.fetchStream(this.props.match.params.id);
   }
 
+  onSubmit = (formValues) => {
+    console.log(formValues);
+  };
+
   render() {
-    console.log(this.props);
     if (!this.props.stream) {
       return <div>Loading...</div>;
     }
-    return <div>{this.props.stream.title}</div>;
+    return (
+      <div>
+        <h3>Edit a Stream</h3>
+        <StreamForm
+          initialValues={{
+            title: this.props.stream.title,
+            description: this.props.stream.description,
+          }}
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
   }
 }
 
@@ -29,4 +35,6 @@ const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit);
+export default connect(mapStateToProps, { fetchStream, editStream })(
+  StreamEdit
+);
