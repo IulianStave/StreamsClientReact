f61b34fee03bff786046aa6565e10713d2bfd57c
IulianStave <iulian.staverescu@gmail.com>
2020-11-23 18:14:30 +0200

    Fix Route configuration

--- src/components/App.js
+++ src/components/App.js
@@ -18,7 +18,7 @@ const App = () => {
         <Route path="/" exact component={StreamList} />
         <Route path="/streams/new" component={StreamCreate} />
         <Route path="/streams/delete/:id" exact component={StreamDelete} />
-        <Route path="/streams/show/:id" exact component={StreamShow} />
+        <Route path="/streams/:id" exact component={StreamShow} />
         <Route path="/streams/edit/:id" exact component={StreamEdit} />
       </Router>
     </div>
