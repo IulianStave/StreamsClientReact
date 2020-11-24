1034c5b67300c15349e16916bc44ecfc0049cdbe
IulianStave <iulian.staverescu@gmail.com>
2020-11-24 13:33:17 +0200

    Add lyfe cycle component to show the player

--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { fetchStream } from "../../actions";
 import { connect } from "react-redux";
-import flv from 'flv.js';
+import flv from "flv.js";
 
 class StreamShow extends React.Component {
   constructor(props) {
@@ -10,16 +10,39 @@ class StreamShow extends React.Component {
   }
 
   componentDidMount() {
-    const { id } = this.props.match.params.id;
+    const { id } = this.props.match.params;
+
     this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+  
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  buildPlayer() {
+    // OBS setup: Source: Input, Output Settings:: Service = Custom... 
+    // Server = rtmp://localhost/live 
+    // Key=StreamId
+    if (this.player || !this.props.stream) {
+      return;
+    }
+
+    const { id } = this.props.match.params;
+    
     // set up video player
     this.player = flv.createPlayer({
-      type: 'flv',
-      url: `http://localhost:8000/live/${id}.flv`
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`,
     });
     this.player.attachMediaElement(this.videoRef.current);
     this.player.load();
   }
+
 
   render() {
     if (!this.props.stream) {
@@ -29,7 +52,7 @@ class StreamShow extends React.Component {
     const { title, description } = this.props.stream;
     return (
       <div className="item">
-        <video ref={this.videoRef} controls={true} style={{width: '100%'}}/>
+        <video ref={this.videoRef} controls={true} style={{ width: "100%" }} />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
